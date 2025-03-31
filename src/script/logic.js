7f92d4969689d591df17d3d01e0823b8a6cbab20
PuuTzzA <putzerthomas03@gmail.com>
2025-03-31 23:13:13 +0200

    changed id to name and added all candidates

--- src/script/logic.js
+++ src/script/logic.js
@@ -1,15 +1,32 @@
-const idToLink = {
-    1: { name: "Bernie Sanders", url: "https://youtu.be/rKBM2kS6B8o?si=kF7L-FaVpTuWv7M3" },
-    2: { name: "Jan Bömermann", url: "https://youtu.be/DgPvWtvUZF8?si=0NAU_7tNs8l5hMeA" }
+const nameToLink = {
+    "Mittermair": { name: "Mittermair Friedrich", url: "none yet" },
+    "Ghergu": { name: "Ghergu Leitner Daniela Elena", url: "none yet" },
+    "GruberM": { name: "Gruber Markus", url: "none yet" },
+    "GruberS": { name: "Gruber Stefan", url: "none yet" },
+    "Innerhofer": { name: "Innerhofer Putzer Helene", url: "none yet" },
+    "JesacherC": { name: "Jesacher Christian", url: "none yet" },
+    "JesacherH": { name: "Jesacher Hannes", url: "none yet" },
+    "JesacherK": { name: "Jesacher Kurt Albert", url: "none yet" },
+    "LercherC": { name: "Lercher Claudia", url: "none yet" },
+    "LercherT": { name: "Lercher Thomas", url: "none yet" },
+    "Oberkofler": { name: "Oberkofler Weidacher Monika", url: "none yet" },
+    "Sinner": { name: "Sinner Meinhard", url: "none yet" },
+    "SteinerE": { name: "Steiner Erwin Jakob", url: "none yet" },
+    "SteinerF": { name: "Steiner Florian", url: "none yet" },
+    "Taschler": { name: "Taschler Jasmin", url: "none yet" },
+    "TrenkerA": { name: "Trenker Andreas", url: "none yet" },
+    "TrenkerJ": { name: "Trenker Johannes", url: "none yet" },
+    "Weidacher": { name: "Weidacher Dolores", url: "none yet" }
 }
 
 let params = new URLSearchParams(document.location.search);
 
 for (const [key, value] of params) {
     console.log(`key = ${key}, value = ${value}`)
-    if (key == "id" && value in idToLink) {
-        const item = idToLink[value];
+
+    if (key == "name" && value in nameToLink) {
+        const item = nameToLink[value];
         console.log(`${item.name}\nredirecting to ${item.url}`);
-        window.location.replace(item.url);
+        // window.location.replace(item.url);
     }
 }
